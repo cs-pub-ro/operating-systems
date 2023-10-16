2f0c91394c3f578911172fa424d3636850e673f4
gabrielmocanu <gabrielmocanu@users.noreply.github.com>
2023-10-16 20:16:33 +0000

    deploy: 086b7c81015812c050495d5ed4f46e1a4eb8f247

---
+++ 3/assets/js/dba52744.fdc75d13.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkso=self.webpackChunkso||[]).push([[794],{9629:e=>{e.exports=JSON.parse('{"title":"IO","slug":"/Lab/IO/","permalink":"/operating-systems/3/Lab/IO/","navigation":{"previous":{"title":"Arena","permalink":"/operating-systems/3/Lab/Compute/arena"},"next":{"title":"I/O","permalink":"/operating-systems/3/Lab/IO/overview"}}}')}}]);
