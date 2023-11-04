34cd2c8dcda8c284f9ac49d3cdcd4b63999c20ba
StefanJum <StefanJum@users.noreply.github.com>
2023-11-04 11:10:16 +0000

    deploy: 73891a5195315e9a4483783cba3fdd7dcc8ed8c7

---
+++ 21/assets/js/d6b2963d.7f5aa401.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkso=self.webpackChunkso||[]).push([[1041],{5738:e=>{e.exports=JSON.parse('{"title":"IO","slug":"/Lab/IO/","permalink":"/operating-systems/21/Lab/IO/","navigation":{"previous":{"title":"Arena","permalink":"/operating-systems/21/Lab/Compute/arena"},"next":{"title":"I/O","permalink":"/operating-systems/21/Lab/IO/overview"}}}')}}]);
