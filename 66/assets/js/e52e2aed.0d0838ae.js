40e5a785f4a8fe1d072e2c0ee6f4963f0e723b1f
vladiouz <vladiouz@users.noreply.github.com>
2024-03-16 19:59:01 +0000

    deploy: 9b8b4cd6afe9814a272025da378ed2a72ffcb2ed

---
+++ 66/assets/js/e52e2aed.0d0838ae.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkso=self.webpackChunkso||[]).push([[6834],{7491:a=>{a.exports=JSON.parse('{"title":"Data","slug":"/Lab/Data/","permalink":"/operating-systems/66/Lab/Data/","navigation":{"previous":{"title":"Arena","permalink":"/operating-systems/66/Lab/Software Stack/arena"},"next":{"title":"Data","permalink":"/operating-systems/66/Lab/Data/overview"}}}')}}]);
---
+++ 66/assets/js/ea1953eb.2ba5acaa.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkso=self.webpackChunkso||[]).push([[7326],{1176:e=>{e.exports=JSON.parse('{"title":"IO","slug":"/Lab/IO/","permalink":"/operating-systems/66/Lab/IO/","navigation":{"previous":{"title":"Arena","permalink":"/operating-systems/66/Lab/Compute/arena"},"next":{"title":"I/O","permalink":"/operating-systems/66/Lab/IO/overview"}}}')}}]);
