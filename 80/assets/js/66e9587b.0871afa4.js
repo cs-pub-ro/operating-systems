82d702e46ddbbebbad88058e2fe7ce970b8442c5
teodutu <teodutu@users.noreply.github.com>
2024-09-30 20:35:35 +0000

    deploy: eb0056b7d6aac2c5ecd35b911e181f4e46b8bd86

---
+++ 80/assets/js/66e9587b.0871afa4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkso=self.webpackChunkso||[]).push([[6641],{1737:e=>{e.exports=JSON.parse('{"title":"Compute","slug":"/Lab/Compute/","permalink":"/operating-systems/80/Lab/Compute/","navigation":{"previous":{"title":"Arena","permalink":"/operating-systems/80/Lab/Data/arena"},"next":{"title":"Compute","permalink":"/operating-systems/80/Lab/Compute/overview"}}}')}}]);
