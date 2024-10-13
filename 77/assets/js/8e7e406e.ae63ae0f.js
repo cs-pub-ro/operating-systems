617a2f03ff02a12adbe48fdb391154a073c2037a
Alex-deVis <Alex-deVis@users.noreply.github.com>
2024-10-13 17:00:58 +0000

    deploy: 2f88d24a1a96498b166557acd3517c0463527ee9

---
+++ 77/assets/js/8e7e406e.ae63ae0f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkso=self.webpackChunkso||[]).push([[8237],{9295:e=>{e.exports=JSON.parse('{"title":"Lecture","slug":"/Lecture/","permalink":"/operating-systems/77/Lecture/","navigation":{"previous":{"title":"Lab 5 - Memory Security","permalink":"/operating-systems/77/Data/lab5"},"next":{"title":"Data","permalink":"/operating-systems/77/Lecture/Data"}}}')}}]);
