bd50724a3bc43e91cd5aa3525d1128ce85256cdd
fabianpatras <fabianpatras@users.noreply.github.com>
2024-11-05 09:23:52 +0000

    deploy: 23d174a866e14715a0ad39996260876ab05611ae

---
+++ 121/assets/js/4365b4e5.12084595.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkso=self.webpackChunkso||[]).push([[3695],{354:a=>{a.exports=JSON.parse('{"title":"Data","slug":"/Data/","permalink":"/operating-systems/121/Data/","navigation":{"previous":{"title":"Lab 2 - Library Perspective","permalink":"/operating-systems/121/Software Stack/lab2"},"next":{"title":"Data","permalink":"/operating-systems/121/Data/"}}}')}}]);
